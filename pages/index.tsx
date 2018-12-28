65da49f64df606dff6f7c8d7e842402b7d660d4d
Jani Eväkallio <jani.evakallio@gmail.com>
2018-12-28 17:44:17 +0000

    :hatching_chick: Add apollo to root page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Teaser from "../components/Teaser";
 import Layout from "../components/Layout";
-const Index = () => (
+import withApollo from "../client/withApollo";
+
+const Index = withApollo(() => (
   <Layout header={false}>
     <Teaser />
   </Layout>
-);
+));
 
 export default Index;
